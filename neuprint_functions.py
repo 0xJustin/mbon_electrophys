da87c86f3055b4192df7c38d9afd7fd6dc57465b
0xJustin <justinmjoyce75@gmail.com>
2024-08-07 10:04:02 -0400

    New fns

--- neuprint_functions.py
+++ neuprint_functions.py
@@ -9,9 +9,25 @@ token = json.load(open("neuprint_token.json"))["token"]
 if token == "":
     raise Exception("You need to add your neuprint token to neuprint_token.json")
 try:
-    c = Client('neuprint.janelia.org', dataset='hemibrain:v1.2.1', token=token)
+    c = Client("neuprint.janelia.org", dataset="hemibrain:v1.2.1", token=token)
 except:
-    print("Failed to connect to neuprint. Check your token and make sure you are connected to the internet.")
+    print(
+        "Failed to connect to neuprint. Check your token and make sure you are connected to the internet."
+    )
+
+
+def get_soma_location(bodyId):
+    """
+    Returns the x, y, and z coordinates of the soma for the given body ID.
+    :param client: A neuprint client object.
+    :param bodyId: A string representing the body ID to query.
+    :return: A tuple of floats representing the x, y, and z coordinates of the soma.
+    """
+    soma = c.fetch_custom(
+        f"MATCH (n:Neuron) WHERE n.bodyId={bodyId} RETURN n.somaLocation, n.somaRadius"
+    )
+    return soma["n.somaLocation"].iloc[0]["coordinates"], soma["n.somaRadius"].iloc[0]
+
 
 def get_types_and_bodyIds(supertype="KC"):
     """
@@ -27,7 +43,8 @@ def get_types_and_bodyIds(supertype="KC"):
     q = f" MATCH (a :`hemibrain_Neuron`) WHERE {where} RETURN a.bodyId, a.type"
     lh_table = neuprint.fetch_custom(q)
     return lh_table["a.type"].tolist(), lh_table["a.bodyId"].tolist()
-    
+
+
 def get_all_neurons_presynaptic_to(bodyId, supertype="KC"):
     """
     Returns two lists: a list of neuron types and a list of body IDs for all neurons presynaptic to the given body ID.
@@ -44,6 +61,7 @@ def get_all_neurons_presynaptic_to(bodyId, supertype="KC"):
     lh_table = neuprint.fetch_custom(q)
     return lh_table["a.type"].tolist(), lh_table["a.bodyId"].tolist()
 
+
 def pull_synapses(pre_ids=None, post_id=612371421, n_iter=4, pre_or_post="pre"):
     """
     Returns a pandas DataFrame containing synapse information for the given presynaptic neurons and postsynaptic neuron.
@@ -57,8 +75,8 @@ def pull_synapses(pre_ids=None, post_id=612371421, n_iter=4, pre_or_post="pre"):
     # Uncomment this line if you only want synapses who are geographically inside the mushroom body
     synapses_df = pd.DataFrame()
 
-    if pre_or_post == 'both':
-        directions = ['pre', 'post']
+    if pre_or_post == "both":
+        directions = ["pre", "post"]
     else:
         directions = [pre_or_post]
 
@@ -74,10 +92,12 @@ def pull_synapses(pre_ids=None, post_id=612371421, n_iter=4, pre_or_post="pre"):
     for i in range(n_iter):
         # To break up the requests, I just divide up the list of KCs
         if not pre_ids is None:
-            presynaptic_neurons = pre_ids[i * n_neurons // n_iter : (i + 1) * n_neurons // n_iter]
-        
+            presynaptic_neurons = pre_ids[
+                i * n_neurons // n_iter : (i + 1) * n_neurons // n_iter
+            ]
+
         for direction in directions:
-            if direction == 'pre':
+            if direction == "pre":
                 pre = presynaptic_neurons if not pre_ids is None else None
                 post = post_id
             else:
@@ -87,33 +107,41 @@ def pull_synapses(pre_ids=None, post_id=612371421, n_iter=4, pre_or_post="pre"):
             presynaptic_criteria = neuprint.NeuronCriteria(bodyId=pre)
             postsynaptic_criteria = neuprint.NeuronCriteria(bodyId=post)
             try:
-                synapses = neuprint.fetch_synapse_connections(presynaptic_criteria, postsynaptic_criteria, client=c)
+                synapses = neuprint.fetch_synapse_connections(
+                    presynaptic_criteria, postsynaptic_criteria, client=c
+                )
             except Exception as e:
                 print("ERROR: ", e)
-                print("Failed to get synapses- probably a neuprint timeout, try increasing n_iter")
-                
+                print(
+                    "Failed to get synapses- probably a neuprint timeout, try increasing n_iter"
+                )
+
             synapses_df = synapses_df._append(synapses)
     synapses_df.loc[:, "pre_or_post"] = "post"
     synapses_df.loc[synapses_df["bodyId_pre"] == post_id, "pre_or_post"] = "pre"
     return synapses_df.reset_index()
 
-def pre_syn_coord_dict(synapses, bodyids, factor=8*(10**(-3))):
+
+def pre_syn_coord_dict(synapses, bodyids, factor=8 * (10 ** (-3))):
     coordinates = {}
     for ID in bodyids:
         coordinates[str(ID)] = []
     for i in range(len(synapses)):
-        x = int(synapses.loc[i, 'x_post'])
-        y = int(synapses.loc[i, 'y_post'])
-        z = int(synapses.loc[i, 'z_post'])
+        x = int(synapses.loc[i, "x_post"])
+        y = int(synapses.loc[i, "y_post"])
+        z = int(synapses.loc[i, "z_post"])
         x_scaled = factor * x
         y_scaled = factor * y
         z_scaled = factor * z
-        coordinates[str(synapses.loc[i,'bodyId_pre'])].append([x_scaled, y_scaled, z_scaled])
+        coordinates[str(synapses.loc[i, "bodyId_pre"])].append(
+            [x_scaled, y_scaled, z_scaled]
+        )
 
     lengths = []
     for ID, coords in coordinates.items():
         lengths.append(len(coords))
     return coordinates, lengths
+
 
 def pull_skeleton(body_id, healdist=200, return_kdtree=False, skel_format="pandas"):
     # Skeleton pulling and healing code
@@ -126,7 +154,9 @@ def pull_skeleton(body_id, healdist=200, return_kdtree=False, skel_format="panda
     :param return_kdtree: A boolean indicating whether to return the skeleton as a cKDTree. Default is False.
     :return: A pandas DataFrame representing the skeleton.
     """
-    skel_mbon = neuprint.skeleton.fetch_skeleton(body_id, client=c, heal=healdist, format=skel_format)
+    skel_mbon = neuprint.skeleton.fetch_skeleton(
+        body_id, client=c, heal=healdist, format=skel_format
+    )
     # From https://connectome-neuprint.github.io/neuprint-python/docs/client.html#neuprint.client.Client.fetch_skeleton:
     #  If True, a ‘distance’ column (or edge attribute) will be added to the dataframe (or nx.Graph), indicating the distances from each node to its parent node.
 
@@ -136,9 +166,11 @@ def pull_skeleton(body_id, healdist=200, return_kdtree=False, skel_format="panda
     # https://en.wikipedia.org/wiki/K-d_tree
     if return_kdtree:
         from scipy.spatial import cKDTree
+
         tree = cKDTree(list(zip(skel_mbon["x"], skel_mbon["y"], skel_mbon["z"])))
         return skel_mbon, tree
     return skel_mbon
+
 
 def bfs_relabel(skel):
     """
@@ -149,7 +181,7 @@ def bfs_relabel(skel):
     relabel = {}
     visited = []
     queue = [1]
-    counter = 1 
+    counter = 1
     while queue:
         node_index = queue.pop(0)
         if node_index not in visited:
@@ -159,6 +191,7 @@ def bfs_relabel(skel):
             relabel[node_index] = counter
             counter += 1
     return relabel
+
 
 def relabel_skeleton_swc(skel_graph, skel_df, row_name="rowId", link_name="link"):
     relabel_dict = bfs_relabel(skel_graph)
@@ -171,12 +204,37 @@ def relabel_skeleton_swc(skel_graph, skel_df, row_name="rowId", link_name="link"
         swc += s
     return swc, relabel_dict
 
+
 def match_synapses_to_tree(synapses, skel_graph, relabel_dict):
     from scipy.spatial import cKDTree
 
     skel_graph = nx.relabel_nodes(skel_graph, relabel_dict)
-    skel_coords = np.array([[skel_graph.nodes[i]["x"], skel_graph.nodes[i]["y"], skel_graph.nodes[i]["z"]] for i in skel_graph.nodes])
-    synapses_coords = np.array([[synapses.loc[i, 'x_post'], synapses.loc[i, 'y_post'], synapses.loc[i, 'z_post']] if synapses.loc[i, 'pre_or_post'] == 'post' else [synapses.loc[i, 'x_pre'], synapses.loc[i, 'y_pre'], synapses.loc[i, 'z_pre']] for i in synapses.index])
+    skel_coords = np.array(
+        [
+            [
+                skel_graph.nodes[i]["x"],
+                skel_graph.nodes[i]["y"],
+                skel_graph.nodes[i]["z"],
+            ]
+            for i in skel_graph.nodes
+        ]
+    )
+    synapses_coords = np.array(
+        [
+            [
+                synapses.loc[i, "x_post"],
+                synapses.loc[i, "y_post"],
+                synapses.loc[i, "z_post"],
+            ]
+            if synapses.loc[i, "pre_or_post"] == "post"
+            else [
+                synapses.loc[i, "x_pre"],
+                synapses.loc[i, "y_pre"],
+                synapses.loc[i, "z_pre"],
+            ]
+            for i in synapses.index
+        ]
+    )
     # Create a cKDTree from synapses_coords
     skel_tree = cKDTree(skel_coords)
     # Calculate the distance matrix using synapses_tree and skel_coords
